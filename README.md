caca0a9b5e2fa1b55475a900f0953a5f9cde0187
Jeremy Harris <jharris@rockharbor.org>
2011-09-26 13:48:07 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# RH Mission Viejo
+
+This is the WordPress child theme for RH Mission Viejo. It inherits most of its
+templates and functionality from the __ROCK__HARBOR theme base.
+
+See the base theme for more information.
---
+++ libs/child_theme.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * RH Mission Viejo theme
+ * 
+ * @package rockharbor
+ * @subpackage missionviejo
+ */
+class ChildTheme extends RockharborThemeBase {
+		
+/**
+ * List of options for this theme
+ * 
+ * @var array
+ */
+	protected $themeOptions = array(
+		'slug' => 'mission_viejo',
+		'short_name' => 'Mission Viejo'
+	);
+}
